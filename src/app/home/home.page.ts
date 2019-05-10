15fa40ada6a0583f00bb95f893e3c09519842e6e
Shubhi Puntambekar <shubhi.puntambekar@xome.com>
2019-05-10 11:45:43 +0000

    Added Fab Icons and Calendar Functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+export class HomePage {
+  @ViewChild('dateTime') sTime;
+  fabFeedback(){
+    console.log("Feedback Button Clicked!!");
+    
+  }
+  fabCalendar(){
+    console.log("Monthly Button Clicked!!");
+    this.sTime.open();
+  }
 }
